0f38ed4e709efe8b5acf54d1391cb130e0ce1dd0
Rabblerouser <phoenix.public@gmail.com>
2020-05-03 23:13:48 +0300

    Minor bugfixes

--- Authenticator.java
+++ Authenticator.java
@@ -32,9 +32,9 @@ class Authenticator {
         //String spotifyURL = "https://accounts.spotify.com/en/authorize?";
         String spotifyURL = Main.access + "/en/authorize?";
         String client_id = "941ca8e7d13d404f97789c3416d47e5f";
-        String redirect_uri = "http:%2F%2Flocalhost:8765";
+        String redirect_uri = "http://localhost:8765";
         String authUrl = spotifyURL + "client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&response_type=code";
-        System.out.println("use this link to request the access code:");
+//        System.out.println("use this link to request the access code:");
         System.out.println(authUrl);
         System.out.println("waiting for code...");
         server.start();
